cca9bebefa182063d73c91306821dc8bc4365ab9
ALEXANDER <msumba83@gmail.com>
2021-09-08 07:04:17 +0200

    Started components

--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ import ReactDom from "react-dom";
 
 
 function Greeting() {
-  return <h4>This is Alex</h4>;
+  return (
+    <div>
+      <h2>This is Alex, I am leaning react.js</h2>;
+    </div>
+  );
   
 }
+
+//BOTH CODES DOES SAME THING
+
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello World");
+// }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
