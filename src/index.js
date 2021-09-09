cc0baeef1128a9db9abc9fc164191260fd329dac
ALEXANDER <msumba83@gmail.com>
2021-09-09 15:24:04 +0200

    added an image

--- src/index.js
+++ src/index.js
@@ -2,15 +2,37 @@ import React from "react";
 import ReactDom from "react-dom";
 
 
-
-function Greeting() {
+// Nested Components, React Tools
+function BooList() {
   return (
-    <div>
-      <h2>This is Alex, I am leaning react.js</h2>;
-    </div>
+    <section>
+    <Book />
+  </section>
   );
-  
 }
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+    </article>
+  );
+}
+
+const Image = () => {
+  return (<img
+    src="https://images-na.ssl-images-amazon.com/images/I/61yM-irmXoS._AC_UL200_SR200,200_.jpg" alt="logo"
+  />
+  );
+}
+
+// const Person = () => {
+//   return <h2>This is Alex, I am leaning react.js</h2>;
+// }
+
+// const Message = () => {
+//   return <p>This is a book list app</p>;
+// }
 
 //BOTH CODES DOES SAME THING
 
@@ -18,4 +40,4 @@ function Greeting() {
 //   return React.createElement("h1", {}, "Hello World");
 // }
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+ReactDom.render(<BooList />, document.getElementById('root'));
