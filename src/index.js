d273b50be2695bbe7ce53a8296aa2ce1bbb33d89
ALEXANDER <msumba83@gmail.com>
2021-09-12 07:57:21 +0200

    added props

--- src/index.js
+++ src/index.js
@@ -4,31 +4,44 @@ import ReactDom from "react-dom";
 //importing CSS
 import "./index.css";
 
+//setup variables
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/515lG1Sx8IL._AC_UY218_.jpg",
+  title: "Things Fall Apart",
+  author: "Alexander Msumba",
+}
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/61Ovdpf-sfL._AC_UY218_.jpg",
+  title: "Going to School",
+  author: "Irene Alexander",
+}
+
 // Nested Components, React Tools
 function BooList() {
   return (
     <div>
-      <h1 className="bookHeading">BOOK RECOMMENDATION SITE</h1>
+      <div>
+        <h1 className="bookHeading">AJ BOOK RECOMMENDATION SITE</h1>
+        <p className="reading">Knowledge Is Power</p>
+        </div>
 
       <section className="booklist">
-        <Book />
+        <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+         <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
       </section>
     </div>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
       <img
-      src="https://m.media-amazon.com/images/I/51n59fieopL._AC_UY218_.jpg"
-      alt="logo"
-      />
-      <h1>Things Fall Apart</h1>
-       <h4>Author: Alexander Msumba</h4>
-      {/* <Image />
-      <Title />
-      <Author /> */}
+     src={props.img} alt="logo"/>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+     
     </article>
   );
 };
